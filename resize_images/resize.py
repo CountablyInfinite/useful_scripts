719d68268d85d6f9b8d32aa0fff55739dbb0fd6f
sib_hef <timemaster@gmx.at>
2019-11-06 12:06:37 +0100

    added comments and updated readme

--- resize_images/resize.py
+++ resize_images/resize.py
@@ -1,8 +1,11 @@
+# python script to resize all images inside the current folder to 1/2 of the original size and export it to ./resized as .jpg in 80% quality.
+# author: countablyinfinite
+
 from PIL import Image
 import os
 
 path=os.path.dirname(os.path.abspath(__file__))
-resize_ratio = 0.5  # where 0.5 is half size, 2 is double size
+resize_ratio = 0.5  # 0.5 for half the size, 2 to double the size
 
 def resize_images():
     dirs = os.listdir(path)
